e6d50fa85a1084828eb1b8610208a727adf03a84
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2026-02-17 20:49:57 +0100

    Time: 1 ms (64.95%) | Memory: 49.9 MB (45.79%) - LeetSync

---
+++ 169-majority-element/majority-element.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+        int read = 0;
+
+        if(nums.Length == 1){
+            return nums[0];
+        }
+
+        while(read < nums.Length){
+            if(count == 0){
+                candidate = nums[read];
+            }
+
+            if(candidate == nums[read]){
+                count++;
+            }
+            else{
+                count--;
+            }
+
+            read++;
+        }
+
+        return candidate;
+    }
+}
